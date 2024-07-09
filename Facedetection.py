2f486e1eeab9cf25d1d16fba987737af7a7137ff
MURARISETTY MAHATHI DEVI <138689332+Mahathidevi@users.noreply.github.com>
2024-07-09 11:23:13 +0530

    Facedetection.py

---
+++ Facedetection.py
@@ -0,0 +1,2 @@
+/*I have already created a face detection system using harcascade file. This clearly detects the face of the driver and alerts him when he is feeling sleepy to prevent from happening accidents. Below one is my github link to this project. 
+https://github.com/Mahathidevi/DriverDrowsinessDetection-using-openCV-and-Keras /*
